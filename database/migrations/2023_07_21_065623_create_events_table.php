d0a3dd3a06f443d6a64eb36a69ba7969157a9186
Ivan Leonidovich <ukrsoftdev@gmail.com>
2023-07-21 11:27:25 +0300

    add migrations

---
+++ database/migrations/2023_07_21_065623_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_title', 200);
+            $table->timestampTz('event_start_date');
+            $table->timestampTz('event_end_date');
+            $table->foreignId('organization_id')
+                ->constrained(table: 'organizations', indexName: 'organization_id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
